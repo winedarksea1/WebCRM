fb16c53dbc3b7891429b3208228ccda8091c70b2
Andrew McGovern <winedark.mcgovern@gmail.com>
2018-02-23 16:47:57 -0600

    first commit

---
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,38 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+// Below is the version of query you need after Hibernate 5.2
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		// TODO Auto-generated method stub
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// create a query 
+		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+		
+		// execute query and get result list 
+		List<Customer> customers = theQuery.getResultList();
+		
+		return customers;
+	}
+
+}
